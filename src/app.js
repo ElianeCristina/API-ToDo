e5b95992ad2206f5df328e287bb09adf891254ce
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-04-20 09:19:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,27 +4,22 @@ const express = require('express') // importação expres
 const app = express() // instanciou express dentro de app
 const port = 7000 // escolhendo a porta
 
-
 const tarefaController = require(`./controllers/tarefa-controller`);
 const usuarioController = require(`./controllers/usuario-controller`);
 
-// resposta
-tarefaController(app);
-usuarioController(app);
-
-//chama aqui a classe
-
 const Usuario = require('./models/usuario-model');
+const Tarefa = require('./models/tarefa-model');
+const bd = require('./infra/bd'); //importar o banco para o arquivo do servidor
+
+// resposta
+tarefaController(app, bd);
+usuarioController(app, bd);
+
 const novoUsuario = new Usuario("Eliane", "eliane@eliane.com", "12345");
 console.log(novoUsuario);
 
-const Tarefa = require('./models/tarefa-model');
 const novaTarefa = new Tarefa("arrumar cozinha", "arrumar sala", "arrumar o quarto");
 console.log(novaTarefa);
-
-//importar o banco para o arquivo do servidor
-const bd = require('../infra/bd');
-
 
 //body-parser - vai fazer a checagem da rota se a porta vai passar, ajuda a passar a req
 app.use(express.json())

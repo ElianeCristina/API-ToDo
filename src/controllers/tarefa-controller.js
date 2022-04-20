cec47da09e9981c3a1012b41669f4175ee9a734a
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-04-20 20:28:54 -0300

    Update tarefa-controller.js

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -10,7 +10,9 @@ app.get ('/tarefa', (req, res) =>{ // get verbo para mostrar
     res.send('Dados de tarefa')
 })
 app.post ('/tarefa', (req, res) =>{ //post verbo para inserir
-    console.log(req.body)
+    //console.log(req.body)
+    const novaTarefa = new Tarefa(re.body.pendente, req.body.fazendo, req.body.concluido);
+    bd.tarefa.push(novaTarefa);
     res.send("<h1>Tarefas</h1>")
 })
 };

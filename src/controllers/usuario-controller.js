4ec4db54b42403b9d77f6818e9b57e7df866c548
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-04-20 20:28:58 -0300

    Update usuario-controller.js

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -8,14 +8,31 @@ module.exports = (app, bd) => {
         res.json({"banco de dados":bd.usuario})
     })
     //Parametros e Rotas
-    app.get ('/usuario/:nome', function(req, res){//fazer uma busca por parametros
-        const nome = req.params.nome
-        
-        /*passa o parametro da validação da requisição para mostrar na busca no insommnia
-        for(let i = 0; i<= bd.length; i++) {
-            res.json({""Usuario encontrado": nome})
-        }*/
+    app.get ('/usuario/:email', function(req, res){//fazer uma busca por parametros
+        const email = req.params.email
+        console.log(email);for(let i = 0; i <= bd.usuario.length; i++){
+            if(bd.usuario[i].id == email){
+                res.send(bd.usuario[i]);
+            }else{
+                res.send("erro")
+            }
+        }
     })
+
+    /*criando a rota e passando o parâmetro email
+app.get(‘/aluno/:email’, function(req, res)
+//Percorre o array para encontrar o parâmetro
+app.get(‘/aluno/:email’, function(req, res){
+const email = req.params.email    //aqui mostra qual a requisição do parametro
+//cria a variavel i e informa q essa variável tem que ser menor q o tamanho do banco e passa uma vez a mais
+for(let i = 0; i <= bd.usuario.length; i++){ //percorre o array do banco
+//coloca a condição para a comparação
+if(bd.usuario[i].email == email){ // compara o parametro
+res.send (bd.usuario[i].email) //envia a resposta q encontrou na comparação
+}}})
+
+
+    */
 
     app.post ('/usuario', (req, res) =>{ // metodo, rota e o callback de ativação
        // console.log(req.body)// corpo da requisição, os dados de json

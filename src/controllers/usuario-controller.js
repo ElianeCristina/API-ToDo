768a3c7d25004ca59cf8297b839c678bdcf04bb4
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-04-20 09:21:02 -0300

    Update usuario-controller.js

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,15 +1,19 @@
 // fazer a importação do bd
 
-const bd = require('../infra/bd');
-const usuario = require ('./models/usuario-model')
+// const bd = require('../infra/bd');
+const Usuario = require('../models/usuario-model')
 
-module.exports = (app) => {
+module.exports = (app, bd) => {
     app.get ('/usuario', (req, res) => {// get verbo para inserir
-        res.send('Dados do usuario')
+        res.json({"banco de dados":bd.usuario})
     })
     app.post ('/usuario', (req, res) =>{ // metodo, rota e o callback de ativação
-        console.log(req.body)// corpo da requisição
-        res.send('<h1>Dados do usuário</h1>')
+       // console.log(req.body)// corpo da requisição, os dados de json
+      //  res.send('<h1>Dados do usuário</h1>')
+        const body = req.body
+        const novoUsuario = new Usuario(req.body.nome, req.body.email, req.body.senha); // passando o modelo para validar
+        bd.usuario.push(novoUsuario);
+        res.json({"novo usuario":novoUsuario})
 })
 
 };

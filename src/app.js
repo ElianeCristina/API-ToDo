aa1ab11f8eb1df5522bb5746f747cc933fcf6498
ElianeCristina <96190678+ElianeCristina@users.noreply.github.com>
2022-04-20 11:20:14 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ const Usuario = require('./models/usuario-model');
 const Tarefa = require('./models/tarefa-model');
 const bd = require('./infra/bd'); //importar o banco para o arquivo do servidor
 
-// resposta
-tarefaController(app, bd);
-usuarioController(app, bd);
+//body-parser - vai fazer a checagem da rota se a porta vai passar, ajuda a passar a req
+app.use(express.json())
+// o middle sempre coloca antes da abertura da porta 
 
 const novoUsuario = new Usuario("Eliane", "eliane@eliane.com", "12345");
 console.log(novoUsuario);
@@ -21,15 +21,16 @@ console.log(novoUsuario);
 const novaTarefa = new Tarefa("arrumar cozinha", "arrumar sala", "arrumar o quarto");
 console.log(novaTarefa);
 
-//body-parser - vai fazer a checagem da rota se a porta vai passar, ajuda a passar a req
-app.use(express.json())
-// o middle sempre coloca antes da abertura da porta 
+// resposta
+tarefaController(app, bd); 
+// aqui eu coloquei para importar o controller e o bd pq serão usados em mais de um arquivo, 
+//qdo vai usar apenas em um arquivo, basta chamar no arquivo q vai usar, por isso q o model não esta chamando aqui
+usuarioController(app, bd);
+
 
 
 // antes de abrir o localhost, tem q dar um node app.js
 app.listen (port, () =>{ // chamando a porta que vai ser usada
     console.log(`Exemple server listening on port ${port}`)
 })
-
-
 
